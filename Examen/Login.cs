749f71a076af135c005034d44fcf373cea88bd56
Octavio <sal.mercurio.azufre@gmail.com>
2017-02-14 22:05:19 -0600

    Algunas pre-funciones de ventas

--- Examen/Login.cs
+++ Examen/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Examen
 {
@@ -15,11 +16,30 @@ namespace Examen
         public Login()
         {
             InitializeComponent();
+
+            //
+            Thread hilo = new Thread(mostrarSplash);
+            hilo.Start();
+            Thread.Sleep(2500);
+            hilo.Abort();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        public void mostrarSplash()
         {
+            Splash sps = new Splash();
+            sps.ShowDialog();
+        }
 
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            MenuPrincipal ventana = new MenuPrincipal();
+            ventana.ShowDialog();
+            Dispose();
         }
     }
 }
--- Examen/MenuPrincipal.cs
+++ Examen/MenuPrincipal.cs
@@ -15,11 +15,83 @@ namespace Examen
         public MenuPrincipal()
         {
             InitializeComponent();
+            textBoxEfectivo.Focus();
+            //textBoxPrecio.Enabled = false;
+            textBoxSubtotal.Enabled = false;
+            textBoxIva.Enabled = false;
+            textBoxTotal.Enabled = false;
+            textBoxCambio.Enabled = false;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBoxPrecio.Clear();
+            textBoxCantidad.Clear();
+            textBoxEfectivo.Clear();
+            textBoxCambio.Clear();
+            textBoxSubtotal.Clear();
+            textBoxIva.Clear();
+            textBoxTotal.Clear();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void textBoxCantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar) || e.KeyChar == (char)Keys.Enter ||
+                (e.KeyChar == 46) || (e.KeyChar == 8))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                MessageBox.Show("Dato no válido. Sólo se permiten números", "Nota",
+                  MessageBoxButtons.OK, MessageBoxIcon.Information);
+                e.Handled = true;
+            }
+        }
+
+        private void btnVenta_Click(object sender, EventArgs e)
+        {
+            Venta operaciones = new Venta();
+
+            //operaciones.venta();
+            //operaciones.setCatetoAdyacente(Convert.ToDouble(textBoxCatetoAdyacente1.Text.ToString()));
+
+            double num1 = 0;
+            double num2 = 0;
+            double total = 0;
+
+            num1 = Double.Parse(textBoxPrecio.Text);
+            num2 = Double.Parse(textBoxCantidad.Text);
+
+            total = num1 * num2;
+            textBoxTotal.Text = Convert.ToDouble(total).ToString();
+        }
+
+        private void btn500_Click(object sender, EventArgs e)
+        {
+            double num1 = 0;
+            double num2 = 0;
+            double total = 0;
+            double cambio = 0;
+
+            num1 = Double.Parse(textBoxPrecio.Text);
+            num2 = Double.Parse(textBoxCantidad.Text);
+
+            total = num1 * num2;
+            cambio = 500 - total;
+            
+            textBoxTotal.Text = Convert.ToDouble(total).ToString();
+            textBoxCambio.Text = Convert.ToDouble(cambio).ToString();
         }
     }
 }
